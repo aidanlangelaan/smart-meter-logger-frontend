98cb1333922ca9fbff6d1b33263652cca59b2bcb
Aidan Langelaan <aidanlangelaan@gmail.com>
2023-10-18 20:54:25 +0200

    main layout fixes

--- src/src/components/layout/layout.tsx
+++ src/src/components/layout/layout.tsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { localStorageProvider } from "@/utility/local-storage-provider";
 import SetPageTitle from "@/hooks/set-page-title";
+import "./layout.style.scss";
 
 interface ILayout {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -30,11 +31,11 @@ export default function Layout(props: ILayout): JSX.Element {
     <div>
       <SetPageTitle />
       <LayoutHeader onMenuButtonClick={toggleMenuMode} />
-      <div id="content" className="flex-container">
+      <div id="page-container">
         <LayoutSidebar menuMode={menuMode} />
         {/* hack to allow error page be rendered inside the layout
             		https://github.com/remix-run/react-router/discussions/9553#discussioncomment-4907035 */}
-        <div id="page-content">{props.outlet ? props.outlet : <Outlet />}</div>
+        <div id="content-container">{props.outlet ? props.outlet : <Outlet />}</div>
       </div>
     </div>
   );
